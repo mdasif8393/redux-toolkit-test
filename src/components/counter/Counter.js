b07554a2d84b7f73be4ca6b62306a39a9cb02fcb
mdasif8393 <md.asif.8393@gmail.com>
2023-02-02 20:14:11 +0600

    complete

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,19 +1,14 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './counterSlice';
 import styles from './Counter.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from '../../features/counter/counterSlice';
 
 export function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
+
+  const dispatch = useDispatch();
+  const count = useSelector(state => state.counter.count);
+  console.log(count)
 
   const incrementValue = Number(incrementAmount) || 0;
 
@@ -21,17 +16,17 @@ export function Counter() {
     <div>
       <div className={styles.row}>
         <button
+        onClick={()=> dispatch(decrement(5))}
           className={styles.button}
           aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
         >
           -
         </button>
         <span className={styles.value}>{count}</span>
         <button
+        onClick={()=> dispatch(increment())}
           className={styles.button}
           aria-label="Increment value"
-          onClick={() => dispatch(increment())}
         >
           +
         </button>
@@ -45,19 +40,19 @@ export function Counter() {
         />
         <button
           className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
+        
         >
           Add Amount
         </button>
         <button
           className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
+        
         >
           Add Async
         </button>
         <button
           className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
+         
         >
           Add If Odd
         </button>
